fa7b52df3aa6006e04c8a4dc5323eeb4bb4edb24
Zach Roth <zach.roth21@gmail.com>
2023-03-28 17:37:47 -0700

    Add version to interface

--- shared/src/events/record-created-event.ts
+++ shared/src/events/record-created-event.ts
@@ -4,6 +4,7 @@ export interface RecordCreatedEvent {
   subject: Subjects.RecordCreated;
   data: {
     id: string;
+    version: number;
     title: string;
     price: number;
     userId: string;
--- shared/src/events/record-updated-event.ts
+++ shared/src/events/record-updated-event.ts
@@ -4,6 +4,7 @@ export interface RecordUpdatedEvent {
   subject: Subjects.RecordUpdated;
   data: {
     id: string;
+    version: number
     title: string;
     price: number;
     userId: string;
