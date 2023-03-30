e0de5ee52fa4451106197843336a15990203ab48
Zach Roth <zach.roth21@gmail.com>
2023-03-29 18:04:51 -0700

    Add create order listener

--- records/src/events/listeners/order-created-listener.ts
+++ records/src/events/listeners/order-created-listener.ts
@@ -2,6 +2,7 @@ import { Message } from "node-nats-streaming";
 import { Listener, OrderCreatedEvent, Subjects } from "@zroygbiv-ors/sharedcode";
 import { queueGroupName } from "./queue-group-name";
 import { Record } from "../../models/record";
+import { RecordUpdatedPublisher } from "../publishers/record-updated-publisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -17,6 +18,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     record.set({ orderId: data.id });
     // save record
     await record.save();
+    await new RecordUpdatedPublisher(this.client).publish({
+      id: record.id,
+      price: record.price,
+      title: record.title,
+      userId: record.userId,
+      orderId: record.orderId,
+      version: record.version
+    });
+    
     // ack message
     msg.ack();
   }
