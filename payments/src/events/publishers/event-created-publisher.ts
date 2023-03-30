e62cde8b2493106f836a24b6955161d2eac14059
Zach Roth <zach.roth21@gmail.com>
2023-03-30 14:52:19 -0700

    Add payment created publisher

---
+++ payments/src/events/publishers/event-created-publisher.ts
@@ -0,0 +1,5 @@
+import { Subjects, Publisher, PaymentCreatedEvent } from "@zroygbiv-ors/sharedcode";
+
+export class PaymentCreatedPublisher extends Publisher<PaymentCreatedEvent> {
+  subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
+}
--- payments/src/routes/new-charge.ts
+++ payments/src/routes/new-charge.ts
@@ -11,6 +11,8 @@ import {
 } from '@zroygbiv-ors/sharedcode';
 import { Order } from '../models/order';
 import { Payment } from '../models/payment';
+import { PaymentCreatedPublisher } from '../events/publishers/event-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -54,8 +56,13 @@ router.post('/api/payments',
     });
     await payment.save();
 
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId
+    });
 
-    res.status(201).send({ success: true });
+    res.status(201).send({ id: payment.id });
   }
 );
 
