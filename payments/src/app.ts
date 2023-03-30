fead77e749a78e35df13eaf78c2c297ae8a777c4
Zach Roth <zach.roth21@gmail.com>
2023-03-30 12:45:40 -0700

    Update payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUser, errorHandler, NotFoundError } from '@zroygbiv-ors/sharedcode'
+
+// instantiation of express application
+const app = express();
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
+
+app.use(currentUser);
+
+// for routing all types of HTTP requests
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+// bind error handler middleware to app object
+app.use(errorHandler);
+
+export { app };
